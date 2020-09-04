1ca724ba33a71e235af2393f0282fd0711e28cad
yi-tuan <1260320217@qq.com>
2020-09-04 11:21:13 +0800

    feat(question): add #89 - Required Keys & #90 - Optional Keys
    Signed-off-by: yi-tuan <1260320217@qq.com>

---
+++ questions/89-hard-required-keys/test-cases.ts
@@ -0,0 +1,7 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+    Expect<Equal<RequiredKeys<{ foo: number, bar?: string }>, "foo">>,
+    Expect<Equal<RequiredKeys<{ foo: undefined, bar?: undefined }>, "foo">>,
+    Expect<Equal<RequiredKeys<{ foo: undefined, bar?: undefined, title: string, child: null }>, "foo" | "title" | "child">>
+]
---
+++ questions/90-hard-optional-keys/test-cases.ts
@@ -0,0 +1,7 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+    Expect<Equal<OptionalKeys<{ foo: number, bar?: string }>, "bar">>,
+    Expect<Equal<OptionalKeys<{ foo: undefined, bar?: undefined }>, "bar">>,
+    Expect<Equal<OptionalKeys<{ foo: undefined, bar?: undefined, title?: string, child?: null }>, "bar" | "title" | "child">>
+]
